7c92f76fd5b49b21dda5ec187c3084d700aa637d
mkt2320 <mititamakuwala@gmail.com>
2025-05-16 18:00:39 -0400

    Feat: Initial frontend setup with Vite, React, Tailwind and TypeScript

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className="h-screen bg-gray-100 flex items-center justify-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        App is Working!!
+      </h1>
+    </div>
+  );
+}
+export default App;
